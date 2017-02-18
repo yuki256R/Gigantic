7a39b12fd93a6504708f885f5378399a937ba908
BuildTools <unconfigured@null.spigotmc.org>
2017-02-19 00:17:11 +0900

    ちょい足し

--- src/com/github/unchama/hook/GiganticPlaceholders.java
+++ src/com/github/unchama/hook/GiganticPlaceholders.java
@@ -5,19 +5,33 @@ import me.clip.placeholderapi.external.EZPlaceholderHook;
 import org.bukkit.entity.Player;
 
 import com.github.unchama.gigantic.Gigantic;
+import com.github.unchama.gigantic.UserManager;
+import com.github.unchama.player.GiganticPlayer;
 
 public class GiganticPlaceholders extends EZPlaceholderHook{
-
-
+	Gigantic plugin = Gigantic.plugin;
+	
 	public GiganticPlaceholders(Gigantic plugin) {
-		super(plugin, "gigantic");
+		super(plugin, "gp");
 	}
 
 
 	@Override
-	public String onPlaceholderRequest(Player arg0, String arg1) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public String onPlaceholderRequest(Player p, String identifier) {
+		//player型を使わないPlaceholder
+		
+		if(p == null){
+			return "";
+		}
+		GiganticPlayer gp = UserManager.getGiganticPlayer(p);
+		//player型を使うPlaceholder
+		
+		switch(identifier){
+		case "name":
+			return gp.name;
+		default:
+			return "";	
+		}
 	}
 
 }
