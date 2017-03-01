a9089f34f6befd92546f641e37d056181c5c05e6
BuildTools <unconfigured@null.spigotmc.org>
2017-03-01 21:57:27 +0900

    totyuu

--- src/com/github/unchama/listener/SecondListener.java
+++ src/com/github/unchama/listener/SecondListener.java
@@ -5,12 +5,17 @@ import org.bukkit.event.Listener;
 
 import com.github.unchama.event.SecondEvent;
 import com.github.unchama.gigantic.Gigantic;
+import com.github.unchama.gigantic.PlayerManager;
 
 public class SecondListener implements Listener{
 	private Gigantic plugin = Gigantic.plugin;
 	
 	@EventHandler
 	public void GiganticLoadListener(SecondEvent event){
-		
+		//5秒に１回実行する．
+		if(event.getSecond() % 5 != 0){
+			return;
+		}
+		new GiganticLoadTaskRunnable(PlayerManager.waitingmap).
 	}
 }
