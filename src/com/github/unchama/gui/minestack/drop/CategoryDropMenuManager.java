5e40c3c508667a516449111c3d4a42714253dc09
Mon_chi <poyo0315@gmail.com>
2017-03-27 01:29:03 +0900

    MineStack引き出しメニュー外枠

---
+++ src/com/github/unchama/gui/minestack/drop/CategoryDropMenuManager.java
@@ -0,0 +1,7 @@
+package com.github.unchama.gui.minestack.mine.drop;
+
+/**
+ * Created by Mon_chi on 2017/03/25.
+ */
+public class CategoryDropMenuManager {
+}
---
+++ src/com/github/unchama/gui/minestack/farm/CategoryFarmMenuManager.java
@@ -0,0 +1,7 @@
+package com.github.unchama.gui.minestack.farm;
+
+/**
+ * Created by Mon_chi on 2017/03/25.
+ */
+public class CategoryFarmMenuManager {
+}
