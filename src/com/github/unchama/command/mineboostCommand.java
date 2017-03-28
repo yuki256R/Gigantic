1ffdeda91fbed758ad5d39b8cc4290fe48f37465
karayuumac <y-kara0706@docomo.ne.jp>
2017-03-28 16:04:21 +0900

    onTabComplete実装 あっているかどうかわからない...

--- src/com/github/unchama/command/mineboostCommand.java
+++ src/com/github/unchama/command/mineboostCommand.java
@@ -3,6 +3,7 @@ package com.github.unchama.command;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,15 +13,9 @@ import org.bukkit.entity.Player;
 import com.github.unchama.gigantic.Gigantic;
 import com.github.unchama.gigantic.PlayerManager;
 import com.github.unchama.player.GiganticPlayer;
-import com.github.unchama.player.mana.ManaManager;
-import com.github.unchama.player.seichilevel.SeichiLevelManager;
-import com.github.unchama.player.sidebar.SideBarManager;
-import com.github.unchama.player.sidebar.SideBarManager.Information;
 import com.github.unchama.player.mineboost.BoostType;
 import com.github.unchama.player.mineboost.MineBoostManager;
-import com.github.unchama.player.moduler.DataManager;
 import com.github.unchama.util.Converter;
-import com.github.unchama.util.Util;
 import com.github.unchama.yml.ConfigManager;
 
 public class mineboostCommand implements TabExecutor{
@@ -30,171 +25,135 @@ public class mineboostCommand implements TabExecutor{
 
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args) {
-
-		if (args.length <= 0) {
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		/* 
+		 * /mb set <playername/all> <duration(tick)> <amplifier(double)> <id> で実行可能に
+		 * ※ id = 0 その他
+		 *   id = 1 ドラゲナイタイム
+		 *   id = 2 投票
+		 *   
+		 *   No.1 /mb -> usage表示(return false/実行不可)
+		 *   No.2 /mb set -> 使用方法表示 (return true/実行不可)
+		 *   No.3 /mb set <playername/all> -> "効果時間(tick)・上昇値を入力してください"(return true/実行不可)
+		 *   No.4 /mb set <playername/all> <duration(tick)> -> "上昇値を入力してください"(return true/実行不可)
+		 *   No.5 /mb set <playername/all> <duration(tick)> <amplifier(double)> -> "id省略のためプレイヤーには'その他(コマンドなど)による上昇'と表示されます"(return true/実行可能)
+		 *   No.6 全引数指定された完璧な形 -> (return true/実行可能)
+		 */
+		
+		if(args.length == 0){
+			//No.1の場合
 			return false;
 		}
-
-		if (args[0].equalsIgnoreCase("set")) {
-
-			if (args.length <= 1) {
-				sender.sendMessage("設定項目を選んでください．");
+		
+		if(args[0].equalsIgnoreCase("set")){
+			//No.2~6
+			if(args.length == 1){
+				sender.sendMessage(ChatColor.RED + "/mb set <playername/all> <duration(tick)> <amplifier(double)> <id>");
+				sender.sendMessage("指定されたプレイヤーに採掘速度上昇効果を付与します");
+				sender.sendMessage("all指定で全プレイヤー対象");
+				sender.sendMessage("同じ鯖にログイン中の人にしか適用されません");
+				sender.sendMessage("idを指定すると上昇値に説明文を付加出来ます。指定なしだと0が入ります");
+				sender.sendMessage("id=0 その他(コマンドなど)から");
+				sender.sendMessage("id=1 ドラゲナイタイムから");
+				sender.sendMessage("id=2 投票から");
 				return true;
 			}
-
-			if (args[1].equalsIgnoreCase("seichilevel")) {
-
-				if (args.length <= 2) {
-					sender.sendMessage("レベルを指定してください．");
-					return true;
-				}
-
-				if (!(sender instanceof Player)) {
-					sender.sendMessage("ゲーム内で実行してください．");
-					return true;
-				}
-
-				int level = Converter.toInt(args[2]);
-
-				if (level > config.getMaxSeichiLevel()) {
-					sender.sendMessage("最大レベルを超えています．");
-					return true;
-				}
-
-				if(level <= 0){
-					sender.sendMessage("1以上のレベルを指定してください．");
-					return true;
-				}
-
-				Player player = (Player) sender;
-
-				GiganticPlayer gp = PlayerManager.getGiganticPlayer(player);
-
-				if(gp == null){
-					sender.sendMessage("あなたのデータを見つけることができませんでした．");
-					return true;
-				}
-
-				gp.getManager(SeichiLevelManager.class).setLevel(level);
-				gp.getManager(ManaManager.class).setDebugMana();
-				gp.getManager(SideBarManager.class).updateInfo(Information.SEICHI_LEVEL,level);
-				double rb = gp.getManager(SeichiLevelManager.class).getRemainingBlock();
-				gp.getManager(SideBarManager.class).updateInfo(Information.MINE_BLOCK, rb);
-				gp.getManager(SideBarManager.class).refresh();
-				sender.sendMessage("整地レベルを"+ level + "に設定しました．ログアウト時に自動的に解除されます．");
-				return true;
-				
-			}else if(args[1].equalsIgnoreCase("digspped")){
-				//採掘速度上昇コマンド(管理用)
-				//set digspeed player/all duration(tick) amplifier id
-				if(args.length <= 2){
-					sender.sendMessage(ChatColor.RED + "/mb set digspeed <playername/all> <duration(tick)> <amplifier(double)> <id>");
-					sender.sendMessage("指定されたプレイヤーに採掘速度上昇効果を付与します(all指定で全プレイヤー対象)");
-					sender.sendMessage("同じ鯖にログイン中のプレイヤーにしか適用されません");
-					sender.sendMessage("idを指定すると上昇値に説明文を付与できます。指定しないと自動で5が選択されます");
-					sender.sendMessage("id=0　接続人数から");
-					sender.sendMessage("id=1　採掘量から");
-					sender.sendMessage("id=2　ドラゲナイタイムから");
-					sender.sendMessage("id=3　投票から");
-					sender.sendMessage("id=4　その他(不明な上昇値,イベント,不具合等");
-					return true;
-				}else if(args.length == 4 || args.length == 5){
-					//4の時idは省略,5の時はidも
-					BoostType type;
-					if(args.length == 5){
-						//idで場合分け
-						int id = Converter.toInt(args[5]);
-						if(id == 0){
-							type = BoostType.NUMBER_OF_PEOPLE;
-						}else if(id == 1){
-							type = BoostType.MINUTE_MINE;
-						}else if(id == 2){
-							type = BoostType.DRAGON_NIGHT_TIME;
-						}else if(id == 3){
-							type = BoostType.VOTED_BONUS;
-						}else if(id == 4){
-							type = BoostType.OTHERWISE;
-						}else{
-							type = BoostType.OTHERWISE;
-							sender.sendMessage("不明なidが指定されているため、プレイヤーへの説明文には『その他』と表示されます");
-						}
-					}else{
-						//引数が4つ、つまりidが省略されている時
+			if(args.length == 2){
+				//No.3
+				sender.sendMessage("効果時間(tick)・上昇値を入力してください");
+			}
+			if(args.length == 3){
+				//No.4
+				sender.sendMessage("上昇値を入力してください");
+			}
+			if(args.length == 4 || args.length == 5){
+				//No.5,6
+				BoostType type = null;
+				if(args.length == 5){
+					//No.6
+					int num = Converter.toInt(args[4]);
+					if(num == 0){
 						type = BoostType.OTHERWISE;
-						sender.sendMessage("idが指定されていないるため、プレイヤーへの説明文には『その他』と表示されます");
-					}
-					//持続時間を取得
-					long duration = Converter.toLong(args[3]);
-					//effect値を取得
-					short amplifier = Converter.toShort(args[4]);
-					//プレイヤー名をlowercaseする
-					String name = Converter.getName(args[2]);
-					
-					if(!name.equalsIgnoreCase("all")){
-						//プレイヤー名がallじゃない時
-						//プレイヤーをサーバーより取得
-						Player player = plugin.getServer().getPlayer(name);
-						
-						if(player == null){
-							//プレイヤーが取得できない時
-							sender.sendMessage("指定されたプレイヤー(" + name + ")はオンラインではないか、存在しません");
-							return true;
-						}
-						
-						//プレイヤーデータを取得
-						GiganticPlayer gp = PlayerManager.getGiganticPlayer(player);
-						//エラー分岐
-						if(gp == null){
-							player.sendMessage(ChatColor.RED + "playerdataがありません。管理者に報告してください");
-							plugin.getServer().getConsoleSender().sendMessage(ChatColor.RED + "SeichiAssist[コマンドエフェクト付与処理]でエラー発生");
-							plugin.getLogger().warning("playerdataがありません。開発者に報告してください");
-						}
-						//エフェクトデータにこの効果を追加
-						gp.getManager(MineBoostManager.class).updata(type, amplifier, duration);
-						//メッセージ送信
-						sender.sendMessage(ChatColor.LIGHT_PURPLE + name + "に上昇値" + amplifier + "を" + Converter.toTimeString((duration/20)) + "追加しました");
+					}else if(num == 1){
+						type = BoostType.DRAGON_NIGHT_TIME;
+					}else if(num == 2){
+						type = BoostType.VOTED_BONUS;
 					}else{
-						//allの時
-						//全プレイヤーに処理
-						for(GiganticPlayer gp : PlayerManager.gmap.values()){
-							gp.getManager(MineBoostManager.class).updata(type, amplifier, duration);
-						}
-						sender.sendMessage(ChatColor.RED + "全プレイヤーに上昇値" + amplifier + "を" + Converter.toTimeString((duration/20)) + "追加しました");
+						type = BoostType.OTHERWISE;
+						sender.sendMessage("不明なidが指定されているので、idは0が指定されました");
 					}
-					return true;
+				}else{
+					sender.sendMessage("idが指定されていないので、idは0が指定されました");
 				}
-				return false;
+				//持続時間・上昇値をそれぞれ取得
+				long duration = Converter.toLong(args[3]);
+				short amplifier = Converter.toShort(args[4]);
+				//プレイヤー名をlowercaseする
+				String name = Converter.getName(args[2]);
+				
+				if(!name.equalsIgnoreCase("all")){
+					//プレイヤー名がallじゃない時
+					//プレイヤーをサーバーより取得
+					Player player = plugin.getServer().getPlayer(name);
+					
+					if(player == null){
+						//プレイヤーが取得できない時
+						sender.sendMessage("指定されたプレイヤー(" + name + ")はオンラインではないか、存在しません");
+						return true;
+					}
+					
+					//プレイヤーデータを取得
+					GiganticPlayer gp = PlayerManager.getGiganticPlayer(player);
+					//エラー分岐
+					if(gp == null){
+						player.sendMessage(ChatColor.RED + "playerdataがありません。管理者に報告してください");
+						plugin.getServer().getConsoleSender().sendMessage(ChatColor.RED + "SeichiAssist[コマンドエフェクト付与処理]でエラー発生");
+						plugin.getLogger().warning("playerdataがありません。開発者に報告してください");
+						return true;
+					}
+					//エフェクトデータにこの効果を追加
+					gp.getManager(MineBoostManager.class).updata(type, amplifier, duration);
+					//メッセージ送信
+					sender.sendMessage(ChatColor.LIGHT_PURPLE + name + "に上昇値" + amplifier + "を" + Converter.toTimeString((duration/20)) + "追加しました");
+				}else{
+					//allの時
+					//全プレイヤーに処理
+					for(GiganticPlayer gp : PlayerManager.gmap.values()){
+						gp.getManager(MineBoostManager.class).updata(type, amplifier, duration);
+					}
+					sender.sendMessage(ChatColor.RED + "全プレイヤーに上昇値" + amplifier + "を" + Converter.toTimeString((duration/20)) + "追加しました");
+				}
 			}
+			//set以外の文字列の時
+			return false;
 		}
-		return true;
+		return false;
 	}
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command,
-			String label, String[] args) {
-
-        if ( args.length == 1 ) {
-
-            String prefix = args[0].toLowerCase();
-            ArrayList<String> commands = new ArrayList<String>();
-            for ( String c : new String[]{"set"} ) {
-                if ( c.startsWith(prefix) ) {
-                    commands.add(c);
-                }
-            }
-            return commands;
-        }else if( args.length == 2 && args[0].equalsIgnoreCase("set")) {
-
-        	String prefix = args[1].toLowerCase();
-            ArrayList<String> commands = new ArrayList<String>();
-            for ( String c : new String[]{"seichilevel"} ) {
-                if ( c.startsWith(prefix) ) {
-                    commands.add(c);
-                }
-            }
-            return commands;
-        }
-        return null;
+	public List<String> onTabComplete(CommandSender sender, Command command,String label, String[] args) {
+		
+		if(args.length == 1){
+			
+			String prefix = args[0].toLowerCase();
+			ArrayList<String> commands = new ArrayList<String>();
+			for( String c : new String[]{"set"}){
+				if(c.startsWith(prefix)){
+					commands.add(c);
+				}
+			}
+			return commands;
+		}else if(args.length == 2 && args[0].equalsIgnoreCase("set")){
+			
+			String prefix = args[1].toLowerCase();
+			ArrayList<String> commands = new ArrayList<String>();
+			for( Player p : plugin.getServer().getOnlinePlayers()){
+				if(p.getName().startsWith(prefix)){
+					commands.add(p.getName());
+				}
+			}
+			return commands;
+		}
+		return null;
 	}
 }
