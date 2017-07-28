83344be3d6734e35a894194bf7e9622a71ece95b
tar0ss <syas1215@icloud.com>
2017-07-28 23:25:59 +0900

    ExpManagerを正しく修正

--- src/com/github/unchama/player/exp/ExpManager.java
+++ src/com/github/unchama/player/exp/ExpManager.java
@@ -8,12 +8,14 @@ import com.github.unchama.player.moduler.DataManager;
 import com.github.unchama.player.moduler.Initializable;
 import com.github.unchama.player.moduler.UsingSql;
 import com.github.unchama.sql.player.ExpTableManager;
+import com.github.unchama.util.ExperienceManager;
 
 public final class ExpManager extends DataManager implements UsingSql,Initializable{
 
 	ExpTableManager tm;
 
-	private float exp = 0;
+	private ExperienceManager eM;
+	private double exp = 0.0;
 
 	public ExpManager(GiganticPlayer gp) {
 		super(gp);
@@ -25,27 +27,29 @@ public final class ExpManager extends DataManager implements UsingSql,Initializa
 		tm.save(gp,loginflag);
 	}
 
-	/**
-	 * @return exp
-	 */
-	public float getExp() {
-		return exp;
-	}
-
-	/**
-	 * @param exp セットする exp
-	 */
-	public void setExp(float exp) {
-		this.exp = exp;
-	}
-
 	@Override
 	public void init() {
 		Player player = PlayerManager.getPlayer(gp);
-
 		if(player == null)return;
-
-		player.setExp((float)exp);
+		eM = new ExperienceManager(player);
+		eM.setExp(exp);
 	}
 
+
+	public void setExp(float exp) {
+		this.exp = (double)exp;
+	}
+
+	public int getExp() {
+		return eM.getCurrentExp();
+	}
+
+
+	public void changeExp(double exp){
+		eM.changeExp(exp);
+	}
+
+	public ExperienceManager getExperienceManager() {
+		return eM;
+	}
 }
