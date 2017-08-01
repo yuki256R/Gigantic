0f5388f2d6be079f8ab532529c414059952c9c35
tar0ss <syas1215@icloud.com>
2017-08-02 01:33:09 +0900

    がちゃりんごを大きいものに順次交換できるようにしたがバグあり

--- src/com/github/unchama/item/GiganticItem.java
+++ src/com/github/unchama/item/GiganticItem.java
@@ -8,16 +8,15 @@ import com.github.unchama.item.moduler.ManaEffect;
 
 public enum GiganticItem {
 	ORE_TICKET(new OreTicket()),
-	MANA_FULL(new ManaApple("流星林檎", ManaEffect.MANA_FULL, (short)1).getTaggedApple()),
-	MANA_SMALL(new ManaApple("林檎(小)", ManaEffect.MANA_SMALL, (short)0).getTaggedApple()),
-	MANA_MEDIUM(new ManaApple("林檎(中)", ManaEffect.MANA_MEDIUM, (short)0).getTaggedApple()),
-	MANA_LARGE(new ManaApple("林檎(大)", ManaEffect.MANA_LARGE, (short)0).getTaggedApple()),
-	MANA_HUGE(new ManaApple("林檎(極)", ManaEffect.MANA_HUGE, (short)0).getTaggedApple()),
-	;
+	MANA_FULL(new ManaApple("流星林檎", ManaEffect.MANA_FULL, (short) 1).getTaggedApple()),
+	MANA_SMALL(new ManaApple("林檎(小)", ManaEffect.MANA_SMALL, (short) 0).getTaggedApple()),
+	MANA_MEDIUM(new ManaApple("林檎(中)", ManaEffect.MANA_MEDIUM, (short) 0).getTaggedApple()),
+	MANA_LARGE(new ManaApple("林檎(大)", ManaEffect.MANA_LARGE, (short) 0).getTaggedApple()),
+	MANA_HUGE(new ManaApple("林檎(極)", ManaEffect.MANA_HUGE, (short) 0).getTaggedApple()), ;
 
 	private final ItemStack is;
 
-	GiganticItem(ItemStack is){
+	GiganticItem(ItemStack is) {
 		this.is = is;
 	}
 
