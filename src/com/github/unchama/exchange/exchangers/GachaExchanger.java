8453846b05f07e12a7d09afd2a7709db1462491b
tar0ss <syas1215@icloud.com>
2017-08-03 19:30:20 +0900

    交換できるものをOLDガチャと現GIGANTICガチャのみに設定

--- src/com/github/unchama/exchange/exchangers/GachaExchanger.java
+++ src/com/github/unchama/exchange/exchangers/GachaExchanger.java
@@ -1,6 +1,7 @@
 package com.github.unchama.exchange.exchangers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,83 +25,90 @@ import de.tr7zw.itemnbtapi.NBTItem;
 
 public final class GachaExchanger implements Exchanger {
 
-    //レアリティに対してガチャ券の配布枚数
-    private Map<Rarity, Integer> ticketMap;
+	private static final List<GachaType> canExchangeType = new ArrayList<>(Arrays.asList(GachaType.GIGANTIC,
+			GachaType.OLD));
 
-    public GachaExchanger() {
-        this.ticketMap = new HashMap<>();
-        ticketMap.put(Rarity.BIG, 12);
-        ticketMap.put(Rarity.WIN, 3);
-    }
+	//レアリティに対してガチャ券の配布枚数
+	private Map<Rarity, Integer> ticketMap;
 
-    @Override
-    public void exchange(Player player, ItemStack[] input) {
-        //余剰アイテム用リスト
-        List<ItemStack> outputItems = new ArrayList<>();
+	public GachaExchanger() {
+		this.ticketMap = new HashMap<>();
+		ticketMap.put(Rarity.BIG, 12);
+		ticketMap.put(Rarity.WIN, 3);
+	}
 
-        //レアリティ別アイテム数マップ
-        Map<Rarity, Integer> countMap = new HashMap<>();
-        for (ItemStack item : input) {
-            if (item == null) continue;
+	@Override
+	public void exchange(Player player, ItemStack[] input) {
+		//余剰アイテム用リスト
+		List<ItemStack> outputItems = new ArrayList<>();
 
-            NBTItem nbti = new NBTItem(item);
+		//レアリティ別アイテム数マップ
+		Map<Rarity, Integer> countMap = new HashMap<>();
+		for (ItemStack item : input) {
+			if (item == null)
+				continue;
 
-            if(!nbti.hasKey(Gacha.GACHARARITYNBT) || !nbti.hasKey(Gacha.ROLLPLAYERUUIDNBT)){
-            	outputItems.add(item);
-            	continue;
-            }
-            Rarity r = GachaManager.getGachaRarity(nbti);
-            UUID uuid = GachaManager.getUUID(nbti);
-            //対象アイテムはカウント
-            if (ticketMap.containsKey(r) && uuid != null && uuid.equals(player.getUniqueId())) {
-                Integer result = countMap.putIfAbsent(r, item.getAmount());
-                if (result != null) {
-                    countMap.put(r, result + item.getAmount());
-                }
-            }
-            //非対象アイテムは余剰アイテムリストへ
-            else {
-                outputItems.add(item);
-            }
-        }
+			NBTItem nbti = new NBTItem(item);
 
-        //カウントしたレアリティからチケット枚数を計算
-        int outputTickets = 0;
-        for (Rarity r : countMap.keySet()) {
-            int inputAmount = countMap.get(r);
-            int ticketAmount = ticketMap.get(r);
-            outputTickets += ticketAmount * inputAmount;
-        }
+			if (!nbti.hasKey(Gacha.GACHATYPENBT) || !nbti.hasKey(Gacha.GACHARARITYNBT)
+					|| !nbti.hasKey(Gacha.ROLLPLAYERUUIDNBT)) {
+				outputItems.add(item);
+				continue;
+			}
+			GachaType gtype = GachaManager.getGachaType(nbti);
+			Rarity r = GachaManager.getGachaRarity(nbti);
+			UUID uuid = GachaManager.getUUID(nbti);
+			//対象アイテムはカウント
+			if (canExchangeType.contains(gtype) && ticketMap.containsKey(r) && uuid != null
+					&& uuid.equals(player.getUniqueId())) {
+				Integer result = countMap.putIfAbsent(r, item.getAmount());
+				if (result != null) {
+					countMap.put(r, result + item.getAmount());
+				}
+			}
+			//非対象アイテムは余剰アイテムリストへ
+			else {
+				outputItems.add(item);
+			}
+		}
 
-        //チケット付与
-        for (int i = 0; i < outputTickets / 64; i++) {
-            giveTicket(player, 64);
-        }
-        giveTicket(player, outputTickets % 64);
+		//カウントしたレアリティからチケット枚数を計算
+		int outputTickets = 0;
+		for (Rarity r : countMap.keySet()) {
+			int inputAmount = countMap.get(r);
+			int ticketAmount = ticketMap.get(r);
+			outputTickets += ticketAmount * inputAmount;
+		}
 
+		//チケット付与
+		for (int i = 0; i < outputTickets / 64; i++) {
+			giveTicket(player, 64);
+		}
+		giveTicket(player, outputTickets % 64);
 
-        for (Rarity r : countMap.keySet()) {
-            int inputAmount = countMap.get(r);
-            int ticketAmount = ticketMap.get(r);
-            player.sendMessage(ChatColor.GREEN + r.getRarityName() + "の景品 " + inputAmount + "個  × " + ticketAmount + " = " + ticketAmount * inputAmount + "枚");
-        }
-        player.sendMessage(ChatColor.GREEN + "合計取得ガチャ券：  " + outputTickets + "枚");
+		for (Rarity r : countMap.keySet()) {
+			int inputAmount = countMap.get(r);
+			int ticketAmount = ticketMap.get(r);
+			player.sendMessage(ChatColor.GREEN + r.getRarityName() + "の景品 " + inputAmount + "個  × " + ticketAmount
+					+ " = " + ticketAmount * inputAmount + "枚");
+		}
+		player.sendMessage(ChatColor.GREEN + "合計取得ガチャ券：  " + outputTickets + "枚");
 
-        //余剰アイテム付与
-        for (ItemStack item : outputItems) {
-            Util.giveItem(player, item, false);
-        }
-        player.sendMessage(ChatColor.GREEN + "余りアイテムと対象外アイテムを返却しました");
-    }
+		//余剰アイテム付与
+		for (ItemStack item : outputItems) {
+			Util.giveItem(player, item, false);
+		}
+		player.sendMessage(ChatColor.GREEN + "余りアイテムと対象外アイテムを返却しました");
+	}
 
-    /***
-     *
-     * @param player チケットを付与するプレイヤー
-     * @param amount 付与するチケットの枚数
-     */
-    private void giveTicket(Player player, int amount) {
-        GiganticPlayer gp = PlayerManager.getGiganticPlayer(player);
-        PlayerGachaManager pgM = gp.getManager(PlayerGachaManager.class);
-        pgM.give(player, GachaType.GIGANTIC, amount);
-    }
+	/***
+	 *
+	 * @param player チケットを付与するプレイヤー
+	 * @param amount 付与するチケットの枚数
+	 */
+	private void giveTicket(Player player, int amount) {
+		GiganticPlayer gp = PlayerManager.getGiganticPlayer(player);
+		PlayerGachaManager pgM = gp.getManager(PlayerGachaManager.class);
+		pgM.give(player, GachaType.GIGANTIC, amount);
+	}
 }
